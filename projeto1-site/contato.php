14acb6d81627ba26dac98bb7d0c30dccc646c3ce
Fernando Reis <fernando@redelocal.net>
2015-01-15 16:00:03 -0200

    Alteração no contato.php

--- projeto1-site/contato.php
+++ projeto1-site/contato.php
@@ -7,28 +7,28 @@
   <div class="control-group">
     <label class="control-label" for="nome">Nome</label>
     <div class="controls">
-    <input type="text" id="nome" placeholder="Digite seu nome completo" required="">
+    <input type="text" id="nome" name="nome" placeholder="Digite seu nome completo" required="">
     </div>
   </div>
 
   <div class="control-group">
     <label class="control-label" for="email">Email</label>
     <div class="controls">
-      <input type="text" id="email" placeholder="" required="">
+      <input type="text" id="email" name="email" placeholder="" required="">
     </div>
   </div>
   
   <div class="control-group">
    <label class="control-label" for="assunto">Assunto</label>
    <div class="controls">
-     <input type="text" id="assunto" placeholder="" required="">
+     <input type="text" id="assunto" name="assunto" placeholder="" required="">
    </div>
  </div> 
 
  <div class="control-group">
     <label class="control-label" for="mensagem">Mensagem</label>
     <div class="controls">
-    <textarea rows="5" id="mensagem" placeholder="Digite a sua mensagem"> </textarea>   
+    <textarea rows="5" id="mensagem" name="mensagem" placeholder="Digite a sua mensagem"> </textarea>   
     </div>
   </div>
 
@@ -41,3 +41,33 @@
   </div>
 </fieldset>
 </form>    
+
+
+<?php
+
+if(isset($_POST['nome']) || isset($_POST['email']) || isset($_POST['assunto']) || isset($_POST['mensagem']))
+{
+
+echo "<div class=\"row\">
+	<div class=\"span9 offset3\"><h4>Dados enviados com sucesso, abaixo seguem os dados que voce enviou.</h4></div></div>";
+echo "
+<div class=\"row\">
+  <div class=\"span1 offset3\">Nome:</div>
+  <div class=\"span3 offset\">".$_POST['nome']."</div>
+</div>
+<div class=\"row\">
+  <div class=\"span1 offset3\">Email:</div>
+  <div class=\"span3 offset\">".$_POST['email']."</div>
+</div>
+<div class=\"row\">
+  <div class=\"span1 offset3\">Assunto:</div>
+  <div class=\"span3 offset\">".$_POST['assunto']."</div>
+</div>
+<div class=\"row\">
+  <div class=\"span1 offset3\">Mensagem:</div>
+  <div class=\"span3 offset\">".$_POST['mensagem']."</div>
+</div>";
+
+}
+
+?>
